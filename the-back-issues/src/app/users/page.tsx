8b2ec8f837b8eea7a1892e1e7d13fcde757ef617
darkfutrue <bpb9629@gmail.com>
2025-06-26 16:04:26 -0400

    Simple next.js stuff

---
+++ the-back-issues/src/app/users/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+const UsersPage = () => {
+    return (
+        <div><Link href="users/newuser">Click here to go to the new user page</Link></div>
+    )
+}
+
+export default UsersPage
